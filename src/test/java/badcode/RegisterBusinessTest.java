7e42df9bbc12f71055d879d0c7b807597a9f409a
K0966149 <vatcharit.o@kbtg.tech>
2022-02-17 13:41:13 +0700

    badcode: test case03 - case05

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -25,4 +25,61 @@ public class RegisterBusinessTest {
         });
         assertEquals("First name is required.",exception.getMessage());
     }
+
+    @Test
+    @DisplayName("Last name = null ดังนั้นจะโยน exception ออกมา "+
+            "พร้อมกับคำว่า Last name is required.")
+    public void case03() {
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Ball");
+        Exception exception = assertThrows(ArgumentNullException.class, () -> {
+            business.register(null, speaker);
+        });
+        assertEquals("Last name is required.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Email = null ดังนั้นจะโยน exception ออกมา "+
+            "พร้อมกับคำว่า Email is required.")
+    public void case03() {
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Ball");
+        speaker.setLastName("Boba");
+        Exception exception = assertThrows(ArgumentNullException.class, () -> {
+            business.register(null, speaker);
+        });
+        assertEquals("Email is required.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Email ไม่อยู่ใน domains ดังนั้นจะโยน exception ออกมา "+
+            "พร้อมกับคำว่า Speaker doesn't meet our standard rules.")
+    public void case04() {
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Ball");
+        speaker.setLastName("Boba");
+        speaker.setEmail("ballboba@sanook.com");
+        Exception exception = assertThrows(SpeakerDoesntMeetRequirementsException.class, () -> {
+            business.register(null, speaker);
+        });
+        assertEquals("Speaker doesn't meet our standard rules.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Repository is null exception ออกมา "+
+            "พร้อมกับคำว่า Can't save a speaker.")
+    public void case05() {
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Ball");
+        speaker.setLastName("Boba");
+        speaker.setEmail("ballboba@gmail.com");
+        Exception exception = assertThrows(SaveSpeakerException.class, () -> {
+            business.register(null, speaker);
+        });
+        assertEquals("Can't save a speaker.",exception.getMessage());
+    }
 }
