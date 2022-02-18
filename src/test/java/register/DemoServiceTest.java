21938cd82920d7860c2bce274aecc5de137175a6
K0966149 <vatcharit.o@kbtg.tech>
2022-02-18 11:42:37 +0700

    register, unit-test: change class dependency to interface.

--- src/test/java/register/DemoServiceTest.java
+++ src/test/java/register/DemoServiceTest.java
@@ -2,37 +2,20 @@ package register;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//การ Mock Service test ทำได้ สอง แบบ
 class DemoServiceTest {
     @Test
     @DisplayName("Get result 2 from input 1.")
     void case_01() {
-        DB db = new DB() {
-            @Override
-            int increaseOne(int id) {
-                return 2;
-            }
-        };
+        DB db = new DBX();
         DemoService demoService = new DemoService(db);
         String actualResult = demoService.compute(1);
         assertEquals("Result = 2", actualResult);
     }
-
-
-
-    @Test
-    @DisplayName("Get result 3 from input 2.")
-    void case_02() {
-        DB db = new DBX();
-        DemoService demoService = new DemoService(db);
-        String actualResult = demoService.compute(2s);
-        assertEquals("Result = 3", actualResult);
-    }
 }
 
-class DBX extends DB {
+class DBX implements DB {
     @Override
-    int increaseOne(int id) {
-        return 3;
+    public int increaseOne(int id) {
+        return 2;
     }
 }
